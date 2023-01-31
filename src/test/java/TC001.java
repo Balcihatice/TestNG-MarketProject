140e1c0c426d48a22d139e060082580e7e6a09d2
Balcihatice <aciceaybike@gmail.com>
2023-01-31 23:47:47 +0300

    TestNgProject

--- src/test/java/TC001.java
+++ src/test/java/TC001.java
@@ -1,8 +1,19 @@
+import Base.BaseTest;
+import Pages.AllPages;
+import Pages.MainPage;
+import Utilities.ConfigReader;
+import org.testng.annotations.Test;
+public class TC001 extends BaseTest {
 
-public class TC001 {
+    AllPages allPages=new AllPages();
+    @Test
+    public void test01(){
+        allPages.mainPage().go_To_Url(ConfigReader.getProperty("baseURL"));
+        allPages.mainPage().hoverOverGirisYapButon();
+        allPages.loginPage().loginHepsiBurada(ConfigReader.getProperty("UserMail"),ConfigReader.getProperty("UserPassword"));
+        allPages.userDashboardPage().verifyValidLogin("test");
+        allPages.userDashboardPage().searchProduct("robot süpürge");
 
 
-
-
-
+    }
 }
